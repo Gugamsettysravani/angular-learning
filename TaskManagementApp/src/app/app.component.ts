e007b37cf690e7d4b8afd305134a9df97d367874
Gugamsettysravani <gugamsettysravani3@gmail.com>
2025-02-06 19:12:46 +0530

    commit2

--- TaskManagementApp/src/app/app.component.ts
+++ TaskManagementApp/src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
